e4f58716365479e396656c308ef5549d43169d2d
ron-afs <94569277+ron-afs@users.noreply.github.com>
2021-12-13 15:47:44 -0500

    fix menu and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutModule } from './about/about.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () => AboutModule,
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => DashboardModule,
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule, Optional, SkipSelf, ErrorHandler } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HttpClientModule],
+})
+export class CoreModule {}
